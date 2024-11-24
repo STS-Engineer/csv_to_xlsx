19c00cf97747727b1321149c74503a7711882fe7
STS-Engineer <administration.sts@avocarbon.com>
2024-11-24 22:56:19 +0100

    update

--- converter.py
+++ converter.py
@@ -60,6 +60,7 @@ UPLOAD_FORM_HTML = """
       }
     </style>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+      <link rel="icon" href="/static/logo-avocarbon-carre.ico" type="image/x-icon">
   </head>
   <body>
     <div class="container">
